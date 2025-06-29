cc427cbacf76b2dde097869cfebd0040da704f3b
Yael-Bloch <yaelbloch2023@gmail.com>
2025-06-29 13:50:37 +0300

    finish

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -1,159 +1,4 @@
 
-// import bcrypt from "bcrypt";
-// import usersModel from "../modules/user.js";
-// import commentsModel from "../modules/comments.js";
-// import path from "path";
-// import fs from "fs";
-// import { create } from "domain";
-// import booksModel from "../modules/books.js";
-
-
-// const commentsController = {
-//     getAll: async (req, res) => {
-//         try {
-//             const comments = await commentsModel.getAll() || [];
-
-//             const commentsWithImage = comments.map(comment => {
-//                 const id = comment.Id;
-//                 const possibleExtensions = ['.jpg', '.jpeg', '.png', '.webp'];
-
-//                 let imageUrl = null;
-
-//                 for (const ext of possibleExtensions) {
-//                     const imagePath = path.join(picturesDir, `${id}${ext}`);
-//                     if (fs.existsSync(imagePath)) {
-//                         imageUrl = `/comment-images/${id}${ext}`;
-//                         break;
-//                     }
-//                 }
-
-//                 return {
-//                     ...comment,
-//                     imageUrl  // null אם לא קיימת תמונה
-//                 };
-//             });
-
-//             res.json(commentsWithImage);
-
-//         } catch (err) {
-//             console.error(err);
-//             res.status(500).json({ error: 'Failed to fetch comments' });
-//         }
-//     },
-
-//     getById: async (req, res) => {
-//         try {
-//             const comment = await commentsModel.getById(req.params.commentId);
-//             if (!comment) return res.status(404).json({ message: 'comment not found' });
-//             res.json(comment);
-//         } catch (err) {
-//             console.error('Error getting comment by ID:', err);
-//             res.status(500).json({ error: 'Failed to fetch comment' });
-//         }
-//     },
-//     getAllByBookId: async (req, res) => {
-//         const bookId = req.params.bookId;
-//         try {
-//             const comment = await commentsModel.getAllByBookId(bookId);
-//             if (!comment) return res.status(404).json({ message: 'comment not found' });
-//             res.json(comment);
-//         } catch (err) {
-//             console.error('Error getting comment by ID:', err);
-//             res.status(500).json({ error: 'Failed to fetch comment' });
-//         }
-//     },
-//     add: async (req, res) => {
-//         console.log("Adding comment for book ID:", req.params.bookId);
-//         try {
-//             const bookId = req.params.bookId;
-//             const userId = req.user.id;
-//             const { title, content } = req.body;
-//             if (!bookId || !userId || !title || !content) {
-//                 return res.status(400).json({ error: "All required fields must be filled" });
-//             }
-//             const book_Id=await booksModel.getById(bookId);
-//             if (!book_Id) {
-//                 return res.status(404).json({ error: "Book not found" });
-//             }
-//            const Created_At = new Date().toISOString().slice(0, 19).replace('T', ' ');
-//             console.log(Created_At);
-//             const result =  commentsModel.add({
-//                 bookId: bookId, userId: userId, title: title, content: content, Created_At: Created_At
-//             });
-
-//             console.log("sucsses");
-//             res.status(201).json({ message: "commit added successfully", commentId: result.Id });
-//         } catch (err) {
-//             res.status(500).json({ error: "Error adding the order details" });
-//         }
-//     },
-
-//     update: async (req, res) => {
-//         const commentId = req.params.commentId;
-//         const Seller_Id = req.user.id;
-
-//         const {
-//             comment_Name, author, number_Of_Page, Price,
-//             Category, Note, Status, Editing_Date
-//         } = req.body;
-
-//         const validStatuses = ['offered', 'approved', 'available', 'sold'];
-//         if (!validStatuses.includes(Status)) {
-//             return res.status(400).json({ error: "Invalid status" });
-//         }
-
-//         const user = await usersModel.getById(Seller_Id);
-//         if (!user) {
-//             return res.status(400).json({ error: "Seller not found" });
-//         }
-
-//         const previous = await commentsModel.getById(commentId);
-//         if (!previous) {
-//             return res.status(404).json({ error: "comment not found" });
-//         }
-
-//         try {
-//             const updated = await commentsModel.update(commentId, {
-//                 comment_Name: comment_Name || previous.comment_Name,
-//                 author: author || previous.author,
-//                 number_Of_Page: number_Of_Page || previous.number_Of_Page,
-//                 Price: Price || previous.Price,
-//                 Category: Category || previous.Category,
-//                 Note: Note || previous.Note,
-//                 Status: Status || previous.Status,
-//                 Seller_Id: Seller_Id || previous.Seller_Id,
-//                 Editing_Date: Editing_Date || previous.Editing_Date,
-//             });
-
-//             if (!updated) {
-//                 return res.status(404).json({ error: "comment not found to update" });
-//             }
-
-//             res.json({ message: "comment updated successfully" });
-//         } catch (err) {
-//             console.error("Error updating comment:", err);
-//             res.status(500).json({ error: "Failed to update comment" });
-//         }
-//     },
-
-//     delete: async (req, res) => {
-//         const commentId = req.params.commentId;
-
-//         try {
-//             const deleted = await commentsModel.delete(commentId);
-//             if (!deleted) {
-//                 return res.status(404).json({ error: "comment not found to delete" });
-//             }
-
-//             res.json({ message: "comment deleted successfully" });
-//         } catch (err) {
-//             console.error("Error deleting comment:", err);
-//             res.status(500).json({ error: "Failed to delete comment" });
-//         }
-//     }
-// };
-
-// export default commentsController;
 import bcrypt from "bcrypt";
 import usersModel from "../modules/user.js";
 import commentsModel from "../modules/comments.js";
@@ -215,13 +60,14 @@ const commentsController = {
   },
 
   getAllByBookId: async (req, res) => {
-    const { error } = getAllByBookIdSchema.validate(req.params);
-    if (error) return res.status(400).json({ error: error.details[0].message });
+    // const { error } = getAllByBookIdSchema.validate(req.params);
+    // if (error) return res.status(400).json({ error: error.details[0].message });
 
     try {
       const comments = await commentsModel.getAllByBookId(req.params.bookId);
+      console.log(comments)
       if (!comments || comments.length === 0) {
-        return res.status(404).json({ message: 'No comments found for this book' });
+        return res.json(comments||[]);
       }
       res.json(comments);
     } catch (err) {
@@ -240,6 +86,7 @@ const commentsController = {
 
     try {
       const book = await booksModel.getById(bookId);
+      console.log("book",book)
       if (!book) return res.status(404).json({ error: "Book not found" });
 
       const Created_At = new Date().toISOString().slice(0, 19).replace('T', ' ');
@@ -251,8 +98,8 @@ const commentsController = {
         content,
         Created_At
       });
-
-      res.status(201).json({ message: "Comment added successfully", commentId: result.Id });
+console.log("result",result);
+      res.status(201).json({ message: "Comment added successfully", commentId: result.commentId });
     } catch (err) {
       console.error('Error adding comment:', err);
       res.status(500).json({ error: "Error adding the comment" });
